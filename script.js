67f4320932a48e74ae0c544c87022b7bc8595a31
Sudhanshu Bhatt <101445771+Sudhanshubhatt12@users.noreply.github.com>
2024-12-22 19:28:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const progress = document.getElementById("progress");
 const prev = document.getElementById("prev");
 const next = document.getElementById("next");
@@ -8,21 +7,21 @@ let currentActive = 1;
 
 next.addEventListener("click", () => {
   currentActive++;
-  
+
   if (currentActive > circles.length) {
     currentActive = circles.length;
   }
-  
+
   update();
 });
 
 prev.addEventListener("click", () => {
   currentActive--;
-  
+
   if (currentActive < 1) {
     currentActive = 1;
   }
-  
+
   update();
 });
 
@@ -36,8 +35,11 @@ function update() {
   });
 
   const activeCircles = document.querySelectorAll(".circle.active");
+
+  // Update the width of the progress bar
   progress.style.width = ((activeCircles.length - 1) / (circles.length - 1)) * 100 + "%";
 
+  // Disable/Enable buttons based on the current state
   prev.disabled = currentActive === 1;
   next.disabled = currentActive === circles.length;
 }
